832e143073c0b0f0944d3fb550fe4655ca122379
sakovolga <sakovolga@live.com>
2024-09-23 18:17:44 +0200

    add LoggingAspect

--- src/main/java/com/sakovolga/bookstore/exceprionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/sakovolga/bookstore/exceprionHandler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sakovolga.bookstore.exceprionHandler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,17 +10,20 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import javax.naming.AuthenticationException;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AuthenticationException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ResponseEntity<String> handleException(AuthenticationException ex) {
+        log.warn("Authentication failed: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Authentication Failed" + ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<String> handleGenericException(Exception ex) {
+        log.error("An error occurred: {}", ex.getMessage(), ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred" + ex.getMessage());
     }
 }
