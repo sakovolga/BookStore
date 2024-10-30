f2e228b3145dc15869f88ae344765e1e6df97c8c
sakovolga <sakovolga@live.com>
2024-10-30 07:50:22 +0100

    refactor

--- src/main/java/com/sakovolga/bookstore/repository/CartItemRepository.java
+++ src/main/java/com/sakovolga/bookstore/repository/CartItemRepository.java
@@ -11,11 +11,8 @@ import java.util.List;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-//    @Query("SELECT c FROM CartItem c WHERE c.user = :user")
-    List<CartItem> findAllByUser(User user);
 
-//    @Query(value = "SELECT COUNT(*) = :size FROM cart_items WHERE id IN :cartItemIds", nativeQuery = true)
-//    boolean allExists(@Param("cartItemIds") List<Long> cartItemIds, @Param("size") int size);
+    List<CartItem> findAllByUser(User user);
 
     @Query("SELECT c FROM CartItem c WHERE c.id IN :cartItemIds")
     List<CartItem> findAllByIds(@Param("cartItemIds") List<Long> cartItemIds);
